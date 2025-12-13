94217c21389e862a5803d3f8b09b6ad7ff4215dd
Ed Esquivel <edgaresquivel0615@gmail.com>
2025-12-13 12:17:13 -0500

    Add Route Parameters to link products to the product-detail page

---
+++ app/products/[id]/page.tsx
@@ -0,0 +1,17 @@
+import { products } from "@/app/product-data";
+
+type Props = {
+  params: Promise<{ id: string }>;
+};
+
+export default async function ProductDetailPage({ params }: Props) {
+  const { id } = await params;
+
+  const product = products.find((p) => p.id === id);
+
+  if (!product) {
+    return <h1>Product not found</h1>;
+  }
+
+  return <h1>{product.name}</h1>;
+}
