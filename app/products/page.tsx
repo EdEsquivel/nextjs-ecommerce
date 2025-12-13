5d1345a8d6ab3133dbfd48e54d18287bdafb6b34
Ed Esquivel <edgaresquivel0615@gmail.com>
2025-12-13 12:17:13 -0500

    Add not-found page and styling changes

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -4,7 +4,8 @@ import { products } from "../product-data";
 export default function ProductsPage() {
     return (
         <>
-        <h1>Products</h1>
+        <div className="container mx-auto p-8"></div>
+        <h1 className="text-4xl font-bold mb-8">Products</h1>
         <ProductsList products={products} />
         </>
     )
