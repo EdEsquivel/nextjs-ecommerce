422f9ab70121ad8c3d8f460c5c6f83e22a775645
Ed Esquivel <edgaresquivel0615@gmail.com>
2025-12-20 10:49:14 -0500

    Create remove-from-cart Endpoint

--- app/api/users/[id]/cart/route.ts
+++ app/api/users/[id]/cart/route.ts
@@ -54,3 +54,20 @@ export async function POST( request: NextRequest,
       headers: { "Content-Type": "application/json" },
     });
   }
+
+  export async function DELETE( request: NextRequest,
+  context: { params: Promise<{ id: string }> }) {
+    const { id } = await context.params;
+    const body: CartBody = await request.json();
+    const productId = body.productId;
+
+    carts[id] = carts[id] ? carts[id].filter(pid => pid !== productId) : [];
+    const cartProducts = carts[id]
+      .map(pid => products.find(p => p.id === pid))
+      .filter(Boolean);
+
+    return new Response(JSON.stringify(cartProducts), {
+      status: 202,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
